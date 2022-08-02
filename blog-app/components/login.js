0e25ad4cf47ad446a712c7e7f438dab7e27f4363
eagyemang21 <elijahagyemang9@gmail.com>
2022-08-01 20:28:24 -0400

    Added some login

---
+++ blog-app/components/login.js
@@ -0,0 +1,22 @@
+import React, { useState } from 'react';
+
+
+
+class Login {
+    render() {
+ 
+        return (
+            <div>
+                <form>
+ 
+                    <input type="text" name="username"></input>
+                    <input type="text" name='password'></input>
+ 
+                    {/* make a button for the onClick to render webpage */}
+                    <button onClick={() => { console.log('Let me in!') }}>Log In</button>
+                </form>
+ 
+            </div>
+        )
+    }
+}
